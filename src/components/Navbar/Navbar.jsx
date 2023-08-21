f1fcc2a1656ceadd22e057cce761ff87dae7cf13
saransh <sethia.saransh.ss@gmail.com>
2023-08-21 08:17:55 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,27 @@
-import React from "react";
-import Toggle from "../Toggle/Toggle";
-import "./Navbar.css";
-import { Link } from "react-scroll";
-const navbar = () => {
-  return (
-    <div className="n-wrapper" id="Navbar">
-      {/* left */}
-      <div className="n-left">
-        <div className="n-name">Andrew</div>
-        <Toggle />
-      </div>
-      {/* right */}
-      <div className="n-right">
-        <div className="n-list">
-          <ul style={{ listStyleType: "none" }}>
-            <li>
-              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="services" spy={true} smooth={true}>
-                Serivces
-              </Link>
-            </li>
-            <li>
-              <Link to="works" spy={true} smooth={true}>
-                Experience
-              </Link>
-            </li>
-            <li>
-              <Link to="portfolio" spy={true} smooth={true}>
-                Protfolio
-              </Link>
-            </li>
-            <li>
-              <Link to="testimonial" spy={true} smooth={true}>
-                Testimonial
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <Link to="contact" spy={true} smooth={true}>
-        <button className="button n-button">Contact</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+import React from 'react';
+import './Navbar.css';
+import Ahead from '../../img/ahead.png'
 
-export default navbar;
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+        <div className='n-left'>
+           <img src={Ahead} alt="img" className="image" />
+        </div>
+        <div className='n-right'>
+          <div className='n-list'>
+            <ul type="none">
+                <li>Emotions</li>
+                <li>Manifesto</li>
+                <li>Self-awareness test</li>
+                <li>Work with us</li>
+            </ul>
+          </div>
+          <button className="button">Download App</button>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
+
